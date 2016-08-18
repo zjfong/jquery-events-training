88ca98f68e71c28e1df229d74419cdfb8d4d43cd
Zachary Fong <fong.zachary@gmail.com>
2016-08-18 15:15:06 -0700

    move text and if statement into doc ready

--- starter-code/ready/base.js
+++ starter-code/ready/base.js
@@ -1,16 +1,23 @@
 /*
   What needs to change so that the page says "Go!"?
+
+  Move both text change and if statement into document.ready
 */
 
 $(document).ready(function(){
+  $("h1").text("Go!");
   console.log( "The page says:", $("h1").text() );
-});
 
-$("h1").text("Go!");
 
-if ( $("h1").text() === "Go!" ){
+  if ( $("h1").text() === "Go!" ){
   console.log( "The page says:", $("h1").text() );
   console.log("Success!");
 } else {
   console.log( 'Can you make it say "Go!"?' );
 }
+});
+
+
+
+
+
