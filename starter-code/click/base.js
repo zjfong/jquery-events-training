d409e7151577786dd09f51599e08ecc90735e7a0
Zachary Fong <fong.zachary@gmail.com>
2016-08-18 18:02:03 -0700

    add click event listener on span, append text as li

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,10 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+  $('span').on('click', function track(){
 
+    var text = "You Clicked: " + $(this).text() + " at " + Date.now();
+    //console.log(this);
+    $('ul').append("<li>" + text + "</li>")
+  })
 })
