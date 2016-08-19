a7675b85766cb849512b381e2421e12a1afeb7c8
Zachary Fong <fong.zachary@gmail.com>
2016-08-18 18:32:39 -0700

    add keypress event listener to show stored end minus start time

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,5 +1,22 @@
 console.log("Sanity Check: JS is working!");
 
+var startTime;
+var endTime;
+var total;
+
 $(document).ready(function(){
+  $(window).on("keypress", function startTimer(event){
+    if(event.keyCode === 32){
+
+      if(!startTime){
+        startTime = Date.now();
+      } else {
+        endTime = Date.now();
+        total = (endTime - startTime)/1000;
+        $('#total-time').text(total + " seconds");
+
+      }
+    }
+  })
 
 })
